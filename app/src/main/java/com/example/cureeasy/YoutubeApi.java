c7796f741239de431fb99242735b3840ead100e4
rahulpansari <32799743+rahulpansari@users.noreply.github.com>
2020-01-25 23:09:41 +0530

    Update YoutubeApi.java

--- app/src/main/java/com/example/cureeasy/YoutubeApi.java
+++ app/src/main/java/com/example/cureeasy/YoutubeApi.java
@@ -2,7 +2,7 @@ package com.example.cureeasy;
 
 public class YoutubeApi {
 
-    public static  final String YOUTUBE_API="AIzaSyCz6eNHNW5qstY0XlU8gMaS056q42rVLek";
+    public static  final String YOUTUBE_API="YOUR-API-KEY";
 
     public static final  String YOUTUBE_URL="https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLfuE3hOAeWhYCPPLA75AXfd0pILeyePjv&key=AIzaSyAvnmGEUO5Sc7hxJVMvECRTG5LG0aYEUuk";
 }
